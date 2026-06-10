200a408ef947c55068bd6aad6eb22b39d427440e
gabriel.74903 <gabz.laren@gmail.com>
2026-06-10 16:32:46 -0300

    Iniciando nova tela, MenuScreen.tsx, além de chamar stack no App para importar a navegação de tela, aplicando novos styles e colocando imgs em arrays criado no MenuScreen.tsx, além de importar todas as funcionalidades que vamos usar futuramente na tela

---
+++ screens/MenuScreen.tsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import { View,
+    Text,
+    Image,
+    ScrollView,
+    StyleSheet,
+    TouchableOpacity,
+    StatusBar
+ } from "react-native";
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { RootStackParamList } from "./HomeScreen"; 
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Menu'>;
+
+const combos = [
+    {
+        id: '1',
+        name: 'McOferta Média Big Mac Duplo',
+        description: 'Quatro hambúrgueres (100% carne bovina), alface americana',
+        price: "R$ 39,90",
+        image: require('../images/combo-big-mac-duplo.png'),
+    },
+    {
+        id: '2',
+        name: 'Novo Brabo Melt Onion Rings',
+        description: 'Dois hambúrgueres de carne 100% bovina, méquinese, a exclu...',
+        price: "R$ 41,50",
+        image: require('../images/novo-brabo-melt-onion-rings.png'),
+    },
+    {
+        id: '3',
+        name: 'McCrispy Chicken Elite',
+        description: 'Composto por pão tipo brioche com batata, molho Honey&Fire, ...',
+        price: "R$ 36,20",
+        image: require('../images/combo-mccrispy-chicken-elitex.png'),
+    },
+    {
+        id: '4',
+        name: 'Duplo Cheddar McMelt',
+        description: 'Dois hambúrgueres (100% carne bovina), molho lácteo com queijo...',
+        price: "R$ 36,20",
+        image: require('../images/combo-duplo-cheddar-mcmelt.png'),
+    },
+];
+
+const categories = ['Combos', 'Lanches', 'Fritas', 'Bebidas', 'Sobremesas'];
+
+export default function MenuScreen({ navigation }: Props) {
+    const [activeCategory, setActiveCategory] = useState<string>('Combos');
+    return (
+      
+      );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    scroll: {
+
+    },
+    scrollContent: {
+
+    },
+    headerImageWrapper: {
+
+    },
+    headerImage: {
+
+    },
+    headerButton: {
+
+    },
+    headerButtonLeft: {
+
+    },
+    headerButtonRight: {
+
+    },
+    infoCard: {
+
+    },
+    infoTopRow: {
+
+    },
+    infoLogo: {
+
+    },
+    infoTexts: {
+
+    },
+    infoTitle: {
+
+    },
+    infoSubtitle: {
+
+    },
+    statusRow: {
+
+    },
+    statusText: {
+
+    },
+    categoriesRow: {
+
+    },
+    categoryPill: {
+
+    },
+    categoryPillActives: {
+
+    },
+    categoryText: {
+
+    },
+    categoryTextActive: {
+
+    },
+    sectionTitle: {
+
+    },
+    productRow: {
+
+    },
+    productRowDivider: {
+
+    },
+    productInfo: {
+
+    },
+    productName: {
+
+    },
+    productDescription: {
+
+    },
+    productPrice: {
+
+    },
+    productImage: {
+        
+    },
+});
